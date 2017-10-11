2a0fc8fa59aa6e220407be987d95dd1c518cfea8
selma <selma@gmail.com>
2017-10-11 12:11:09 +0200

    projet front - initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { FormsModule } from '@angular/forms';
+import { HttpModule }  from '@angular/http';
+
+
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
+import { VetementDetailComponent }  from './vetement-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    VetementDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
