11977eea2d989825ad0156165a1f68e5d41a832f
selma <selma@gmail.com>
2017-10-20 17:35:04 +0200

    installation component header et mettre quelques infos sur header html / css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,14 @@
 import { Component } from '@angular/core';
 
 
-export class Vetements {
-  id: number;
-  name: string;
-}
-
-
 @Component({
   selector: 'app-root',
-  template: '<h1>{{title}}</h1><p>{{vetements.title}} details!</p><div><label>id: </label>{{vetement.id}}</div><div> <label>title: </label><input [(ngModel)]="vetement.title" placeholder="name"></div>',
-    
-
-  // templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 
 })
 export class AppComponent {
-  title = 'My first page E-Commerce !';
-  vetements = 'Pantalons';
+  private message:string = 'My first page E-Commerce !';
 }
 
-const VETEMENTS: Vetements[] = [
-  { id: 1, name: 'Pantalons' },
-  { id: 2, name: 'Jupes' },
-  { id: 3, name: 'Robes' },
-  { id: 4, name: 'T-shirts' },
-  { id: 5, name: 'Debardeurs' }
-];
 
